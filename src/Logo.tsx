f915457d69358dc9d94911a300d00e89030bd233
Robert Carter Mills <robcmills@gmail.com>
2024-04-25 21:03:23 -0700

    Add theme song to logo page

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useRef, useState } from 'react'
 import logo from './assets/openspace-jeopardy.jpg'
+import theme from './assets/theme.mp3'
 
 export function Logo() {
-  return <img className='fullscreen logo' src={logo} />
+  const audioRef = useRef<HTMLAudioElement>(null)
+  const [scale, setScale] = useState(0)
+
+  useEffect(() => {
+    const audio = audioRef.current
+    if (audio) {
+      audio.currentTime = 9.5
+      audio.play()
+    }
+    setScale(1)
+  }, [])
+
+  const style = {
+    transition: 'transform 1s',
+    transform: `scale(${scale})`,
+  }
+
+  return (
+    <>
+      <img className='fullscreen logo' src={logo} style={style} />
+      <audio id="theme" controls ref={audioRef}>
+      	<source src={theme} type="audio/mpeg" />
+      </audio>
+    </>
+  )
 }
