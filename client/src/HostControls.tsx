e5d488ba8c564cf391c06d6b19aec997e6b803ac
Robert Carter Mills <robcmills@gmail.com>
2024-06-14 20:57:30 -0700

    Conditionally render host controls

--- client/src/HostControls.tsx
+++ client/src/HostControls.tsx
@@ -1,23 +1,36 @@
 import { CSSProperties } from 'react'
+import { useIsAnyClueVisible } from './useIsAnyClueVisible'
+import { useAtomValue } from 'jotai'
+import { activeContestantAtom } from './activeContestantAtom'
 
 const controlsStyle: CSSProperties = {
   borderTop: '1px solid white',
   display: 'grid',
+  gap: 16,
+  gridAutoFlow: 'column',
+  justifyContent: 'center',
   padding: 16,
   placeItems: 'center',
 }
 
 const buttonStyle: CSSProperties = {
-  borderRadius: 32,
+  border: '1px solid',
+  borderRadius: 16,
   cursor: 'pointer',
+  fontWeight: 'bold',
   height: 32,
   width: 64,
 }
 
 export function HostControls() {
+  const activeContestant = useAtomValue(activeContestantAtom)
+  const isAnyClueVisible = useIsAnyClueVisible()
+  if (!isAnyClueVisible || !activeContestant) return null
+
   return (
     <div style={controlsStyle}>
-      <button style={buttonStyle} title='<spacebar>'></button>
+      <button className='correct' style={buttonStyle}>✓</button>
+      <button className='incorrect' style={buttonStyle}>✗</button>
     </div>
   )
 }
