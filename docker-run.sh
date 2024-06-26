170b3680af63879d5f7288a34c43a7dd893d0c4d
Robert Carter Mills <robcmills@gmail.com>
2024-06-25 21:28:00 -0700

    Update prod script for vercel docker image

--- docker-run.sh
+++ docker-run.sh
@@ -2,5 +2,5 @@ docker run --rm -it \
   -v "$(pwd):/app" \
   -v /app/client/node_modules \
   --workdir /app \
-  node:18-alpine \
-  sh -c "apk add --no-cache bash curl && bash prod.sh"
+  amazonlinux:2023.2.20231011.0 \
+  bash -c "yum update -y && yum install -y unzip && bash ./prod.sh"
