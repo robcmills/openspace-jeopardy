0f88f2098bede2594dbef4f190412334782d49cf
Robert Carter Mills <robcmills@gmail.com>
2024-06-26 07:24:01 -0700

    Update docker image used to oven/bun

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,17 @@
 docker run --rm -it \
   -v "$(pwd):/app" \
   -v /app/client/node_modules \
+  -p 3000:3000 \
   --workdir /app \
-  amazonlinux:2023.2.20231011.0 \
-  bash -c "yum update -y && yum install -y unzip && bash ./prod.sh"
+  --ulimit memlock=-1:-1 \
+  oven/bun \
+  bash -c "./prod.sh"
+
+
+# Vercel image
+# docker run --rm -it \
+#   -v "$(pwd):/app" \
+#   -v /app/client/node_modules \
+#   --workdir /app \
+#   amazonlinux:2023.2.20231011.0 \
+#   bash -c "yum update -y && yum install -y unzip && bash ./prod.sh"
--- prod.sh
+++ prod.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-yum update -y
-yum install -y unzip
-
-echo "Installing bun..."
-curl -fsSL https://bun.sh/install | bash
-export BUN_INSTALL="$HOME/.bun"
-export PATH=$BUN_INSTALL/bin:$PATH
-
 cd client
 echo "Installing client dependencies..."
 bun install
